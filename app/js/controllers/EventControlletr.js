debb2a4fbe394335103ab50deeb85dea1918353f
Samiran <samiran.majumder@gmail.com>
2016-08-24 19:59:52 +0530

    Adding logic to event detail

--- app/js/controllers/EventControlletr.js
+++ app/js/controllers/EventControlletr.js
@@ -11,7 +11,33 @@
                 address:"Mahaveer Rhythm",
                 city:"Bangalore",
                 provience:"Karnataka"
-            }
+            },
+            Sessions:[
+                {
+                    Name:"Directive Angular JS",
+                    CretaorName:"Sam M",
+                    Duration:"1h 10m",
+                    Lavel:"Advance",
+                    Abstruction:"This Going Be truly amezing",
+                    upVoteCountg:0
+                },
+                {
+                    Name:"Scope fun ",
+                    CretaorName:"Sam MZ",
+                    Duration:"1h 10m",
+                    Lavel:"Advance / O+",
+                    Abstruction:"This Going Be amezing Bad",
+                    upVoteCountg:0
+                 },
+                {
+                    Name:"Directive to Learn Angular",
+                    CretaorName:"Sam MZ",
+                    Duration:"1h 10m",
+                    Lavel:"Advance / O+",
+                    Abstruction:"This Going Be amezing Bad",
+                    upVoteCountg:0
+                }
+            ]
         }
     }
     app.controller('EventControlletr', EventControlletr);
